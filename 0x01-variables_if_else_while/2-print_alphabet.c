6bbafce27b351c3a18a12c1d50a8d5747d03fc20
dinaemadroshdy <dinaemadroshdy@gmail.com>
2023-09-11 16:10:54 -0700

    c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print the alphabet in lowercase
+ * Return: 0(success)
+*/
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
